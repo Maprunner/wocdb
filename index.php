a218bc020395e8553fff1ac9d30e2303d43bc79f
Maprunner <simon@maprunner.co.uk>
2015-07-26 19:16:50 +0100

    Add css cache buster: version 0.2.0

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ if (strpos($s['SERVER_NAME'], 'localhost') !== FALSE) {
   $f3->set('DEBUG', 0);
 }
 $f3->set('minFileModTime', filemtime(dirname(__FILE__).'/js/wocdb.min.js'));
+$f3->set('cssFileModTime', filemtime(dirname(__FILE__).'/css/wocdb.css'));
 $f3->config('wocdb.ini');
 $f3->config('wocdb-routes.ini');
 
